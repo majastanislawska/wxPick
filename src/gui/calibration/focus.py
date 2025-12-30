855b21eb579c801f8098e1b5205e4704d3e68fcc
Maja Stanislawska <maja@makershop.ie>
2025-12-30 23:55:02 +0000

    Updated focus module - better climb and uses raw frame.

--- src/gui/calibration/focus.py
+++ src/gui/calibration/focus.py
@@ -13,12 +13,20 @@ panel = None
 parent=None
 btn=None
 running = False
-ranges=list(range(1020, 300, -5))
-results=[]
 last_result =(0,0,0)
 #fill thiis dict with your results
 calibration_table = {
-        1022: -10,
+        895:  -11,
+        835: -10,
+        819:   -9,
+        800:   -8,
+        765:   -7,
+        735:   -6,
+        695:   -5,
+        655:   -4,
+        635:   -3,
+        612:   -2,
+        582:   -1,
         512:    0, # pcb level
         0:     10
     }
@@ -46,69 +54,102 @@ def autofocus_hill_climb(evt):
     global running,btn
     btn.Enable(False)
     running = True
+    results={}
     if not src.gui.panel.topcam.topcam.is_enabled:
         src.gui.panel.topcam.cam_enable(True)
     src.gui.panel.topcam.topcam.canvas_overlays.append(canvas_overlay)
+    src.gui.panel.topcam.topcam.set_autoproperty(2,True) #autofocus
+    src.gui.panel.topcam.topcam.set_autoproperty(1,True) #autoexposure
+    src.gui.panel.topcam.topcam.set_autoproperty(4,True) #auto_whitebalance
     src.gui.panel.topcam.topcam.set_autoproperty(2,False) #autofocus
     src.gui.panel.topcam.topcam.set_autoproperty(1,False) #autoexposure
     src.gui.panel.topcam.topcam.set_autoproperty(4,False) #auto_whitebalance
-    src.gui.panel.topcam.topcam.set_property(2,0) # focus on infinity
-    wx.CallLater(100, _hill_climb_step, pos=0, step=200, direction=1, best_score=-1, iter=0)
+    src.gui.panel.topcam.topcam.set_property(1,625) # exposure time in ms
+    src.gui.panel.topcam.topcam.set_property(2,1023) # focus to reset?
+    src.gui.panel.topcam.topcam.set_property(4,4800) # whitebalance
+    # src.gui.panel.topcam.topcam.set_property(7,41) #contrast
+    # src.gui.panel.topcam.topcam.set_property(11,8) #sharpness
+    wx.CallLater(500, _hill_climb_step, pos=[0], step=128, best_score=-1, results=results)
 
-def _hill_climb_step( pos, step, direction, best_score, iter):
-    if step<1: #no point going sub resolution
-        finalize_focus(pos)
-        return
-    src.gui.panel.topcam.topcam.set_property(2,pos)
+def _hill_climb_step( pos, step, best_score, results):
+    logger.info(f"Focus: pos={pos}, step={step}, best_score={best_score}")
+    src.gui.panel.topcam.topcam.set_property(2,pos[-1])
     #two-step to let camera move.
-    wx.CallLater(220, _evaluate_step, pos, step, direction, best_score, iter)
+    wx.CallLater(220, _evaluate_step, pos, step, best_score, results)
 
-def _evaluate_step(pos, step, direction, best_score, iter):
+def _evaluate_step(pos, step, best_score, results):
     global last_result
-    frame = src.gui.panel.topcam.topcam.get_frame()
+    cur_pos= pos[-1]
+    frame = src.gui.panel.topcam.topcam.get_raw_frame()
+    time.sleep(0.1)  # wait before next frame grab
+    frame =cv2.addWeighted(frame,0.5,src.gui.panel.topcam.topcam.get_raw_frame(),0.5,0)
+    # frame = cv2.fastNlMeansDenoisingColored(frame, None, 10, 10, 7, 15)
+    # cv2.imshow("autofocus input", frame)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    w2=int(src.gui.panel.topcam.topcam.frame_w/2)
-    h2=int(src.gui.panel.topcam.topcam.frame_h/2)
+    cx=int(src.gui.panel.topcam.topcam.raw_optical_cx)
+    cy=int(src.gui.panel.topcam.topcam.raw_optical_cy)
     #half of size of square to be analyzed
     s=int(src.gui.panel.topcam.topcam.frame_h/4)
-    ROI = numpy.s_[h2-s:h2+s, w2-s:w2+s]
+    ROI = numpy.s_[cy-s:cy+s, cx-s:cx+s]
     roi = gray[ROI]
+    # cv2.imshow("autofocus roi", roi)
     #pick algo
-    (score, heatmap)=brenner(roi)
+    # (score, heatmap)=brenner(roi)
     # (score, heatmap)=tenengrad(roi)
     # (score, heatmap)=variance_of_laplacian(roi)
-    # (score, heatmap)=energy_of_laplacian(roi)
+    (score, heatmap)=energy_of_laplacian(roi)
     # (score, heatmap)=normalized_variance(roi)
     # (score, heatmap)=canny_edge_density(roi)
     heatmap_color = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
-    full_heatmap = numpy.zeros_like(frame)
-    full_heatmap[ROI] = heatmap_color
-    src.gui.panel.topcam.topcam.set_frameoverlay(full_heatmap)
-    logger.info(f"Pos {pos:4d} → score {score:8.3f} best {best_score:8.3f} {step} {direction}")
-    last_result=(pos, 0, score)
+    # cv2.imshow("autofocus heatmap", heatmap_color)
+    # full_heatmap = numpy.zeros_like(frame)
+    #we need to place heatmap into full frame for undistortion to work properly
+    frame[ROI] = heatmap_color
+    # cv2.imshow("autofocus full heatmap", full_heatmap)
+    undistorted_heatmap = src.gui.panel.topcam.topcam.undistort_frame(frame)
+    # cv2.imshow("autofocus undistorted heatmap", undistorted_heatmap)
+    src.gui.panel.topcam.topcam.set_frameoverlay(undistorted_heatmap)
+    logger.info(f"Pos {pos} cur:{cur_pos:4d} → score {score:8.3f} best {best_score:8.3f} {step}")
+    last_result=(cur_pos, 0, score)
+    if cur_pos in results:  results[cur_pos].append(score)
+    else:                   results[cur_pos] = [score]
     if score > best_score:
-        # Keep going in same direction, maybe accelerate
-        # new_step = step * 1.4 if (score > best_score * 1.08) else step
-        new_step=step
-        candidate_pos = pos + int(direction * new_step)
+        # logger.info(f" New best score!")
+        candidate_pos = cur_pos + int(step)
         candidate_pos = max(0, min(1023, candidate_pos))
-        wx.CallLater(220, _hill_climb_step,candidate_pos,new_step,direction,score,iter+1)
+        pos.append(candidate_pos)
+        wx.CallLater(100, _hill_climb_step,pos,step,score,results)
     else:
-        # Reverse direction and shrink step
-        new_step = max(0.5, step * 0.5)   # go below 1 so we can bail out
-        candidate_pos = pos + int(-direction * new_step)
+        if abs(step)<=1: #no point going sub resolution
+            if step>0: #do scan in other direction
+                wx.CallLater(100, _hill_climb_step, pos, -128,best_score,results)
+            else: finalize_focus(cur_pos,best_score,results)
+            return
+        pos.pop()  # remove last (worse) position
+        new_step = step * 0.5
+        candidate_pos = pos[-1] + int(new_step)
         candidate_pos = max(0, min(1023, candidate_pos))
-        wx.CallLater(220, _hill_climb_step, candidate_pos,new_step,-direction,score,iter+1)
+        pos.append(candidate_pos)
+        wx.CallLater(100, _hill_climb_step, pos,new_step,best_score,results)
 
-def finalize_focus(final_pos):
+def finalize_focus(final_pos, best_score,results):
     global running,btn,last_result
+    best_pos = final_pos
+    for p in sorted(results.keys()):
+        scores = results[p]
+        max = None #max(scores)
+        if best_score==max: best_pos=p
+        logger.info(f"pos {p} max={max}, {scores}")
+    if best_pos != final_pos:
+        logger.info(f"AUTOFOCUS: moving to best pos {best_pos} from {final_pos}")
+        final_pos=best_pos
     src.gui.panel.topcam.topcam.set_property(2,final_pos)
     distance = interpolate_distance(final_pos)
     logger.info(f"AUTOFOCUS DONE → pos {final_pos}, distance {distance:.2f} mm")
     btn.Enable(True)
     last_result=(final_pos, distance, None)
     running = False
-    src.gui.panel.topcam.cam_enable(False)
+    # src.gui.panel.topcam.cam_enable(False)
 
 def interpolate_distance(pos):
     positions = sorted(calibration_table.keys())
@@ -178,7 +219,7 @@ def energy_of_laplacian(roi):
     lap = cv2.Laplacian(blurred, cv2.CV_64F)
     score = numpy.sum(lap * lap)
     heatmap = numpy.abs(lap)
-    heatmap = numpy.clip(heatmap / numpy.percentile(heatmap, 99) * 255, 0, 255).astype(numpy.uint8)
+    heatmap = numpy.clip(heatmap / numpy.percentile(heatmap, 95) * 255, 0, 255).astype(numpy.uint8)
     return float(score), heatmap
 
 # ----------------------------------------------------------------
